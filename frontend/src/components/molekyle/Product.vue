4d6fe7912cb1fbec01fe5a0a8c949c139ea33bcc
stev <pedersenstephan@hotmail.com>
2019-02-28 12:30:59 +0100

    Sending a product as prop via router-link

---
+++ frontend/src/components/molekyle/Product.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <p>Dette er et enkelt product!</p>
+        <p>{{ thisproduct}}</p>
+    </div>
+</template>
+ <script>
+ export default {
+     props: {
+         thisproduct: {
+             required: true
+         }
+     }
+ }
+ </script>
+
+ <style>
+ 
+ </style>
+ 
+ 
--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,12 +1,13 @@
 import Products from '../components/organisme/Products.vue';
 import Home from '../Home.vue';
 import Login from '../components/atom/Login.vue';
-import Dashboard from '../components/atom/Dashboard.vue'
+import Dashboard from '../components/atom/Dashboard.vue';
+import Product from '../components/molekyle/Product.vue';
 
 export const routes = [
     { path: '/', component: Home },
     { path: '/products', component: Products },
-	{ path: '/products/:id', component: Products },
+	{ path: '/product/:id', name: 'product', component: Product, props: true },
     { path: '/login', component: Login },
     { path: '/dashboard', component: Dashboard }
 ];

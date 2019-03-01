d4a12e48594565350def957d86bda9d7f2f6b69c
Casper Christensen <aurona88@hotmail.com>
2019-03-02 00:24:28 +0100

    minor update for product

--- frontend/src/components/molekyle/Product.vue
+++ frontend/src/components/molekyle/Product.vue
@@ -1,13 +1,15 @@
 <template>
     <div>
-        <p>Dette er et enkelt product!</p>
-        <p>{{ product }}</p>
+        <p>Dette er et enkelt product!...............</p>
+        <p>{{ product.product_id }}</p>
+        <p>{{ product.product_name }}</p>
     </div>
 </template>
  <script>
  export default {
      props: {
          product: {
+             type: Array,
              required: true
          }
      }

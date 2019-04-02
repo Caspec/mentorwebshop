3d81ce7f2a46a314d0bf7498929332ef48bbee73
Casper Christensen <aurona88@hotmail.com>
2019-04-02 22:25:27 +0200

    updated footer with css and footer images

--- frontend/src/components/atom/Footer.vue
+++ frontend/src/components/atom/Footer.vue
@@ -1,6 +1,28 @@
 <template>
 <div class="footer">
-    <p>Dette er Sjakal A/S copyright</p>
+    <p class="margin bolder">Dette er Sjakal A/S copyright &copy; &reg; &trade;</p>
+    <div class="margin">
+        <img src="../../assets/fbicon.png" alt="fbicon" class="imgmargin">
+        <img src="../../assets/yticon.png" alt="yticon" class="imgmargin">
+        <img src="../../assets/twicon.png" alt="twicon" class="imgmargin">
+        <img src="../../assets/snicon.png" alt="snicon" class="imgmargin">
+        <img src="../../assets/suicon.png" alt="suicon" class="imgmargin">
+        <img src="../../assets/callicon.png" alt="callicon" class="imgmargin">
+        <img src="../../assets/visaicon.png" alt="visaicon" class="imgmargin border">
+        <img src="../../assets/msicon.png" alt="msicon" class="imgmargin border">
+    </div>
+    <div class="margin">
+        <img src="../../assets/tricon.png" alt="tricon" class="imgmargin border"> <span class="bolder">Free delivery</span>
+        <img src="../../assets/delicon.png" alt="delicon" class="imgmargin border"> <span class="bolder">Easy return of wares</span>
+        <img src="../../assets/calicon.png" alt="calicon" class="imgmargin border"> <span class="bolder">100 days of return</span>
+    </div>
+    <div class="margin">
+        <a href="#">Tech Blog</a> |
+        <a href="#">Company Information</a> |
+        <a href="#">Trade Conditions</a> |
+        <a href="#">Data Protection</a> |
+        <a href="#">Data Collection</a> 
+    </div>
 </div>
     
 </template>
@@ -12,10 +34,23 @@ export default {
 </script>
 
 <style>
-
 .footer{
     margin-top: 3.69em;
 
+}
+.margin {
+    margin-top: 1.5em;
+    margin-bottom: 2em;
+
+}
+.bolder {
+    font-weight: bold;
+}
+.imgmargin {
+    margin-left: 1em;
+}
+.border {
+    border: 4px solid black;
 }
 </style>
 

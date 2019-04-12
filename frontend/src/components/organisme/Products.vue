3c9be29c6bb09679bd8a543c29880d57b950ae54
Casper Christensen <aurona88@hotmail.com>
2019-04-12 21:10:21 +0200

    added list / grid view in Products.vue

--- frontend/src/components/organisme/Products.vue
+++ frontend/src/components/organisme/Products.vue
@@ -1,18 +1,83 @@
 <template>
   <div id="gridContainer">
     <div class="gridItem one">
-      <div v-for="(product, index) in products" :key="index">
-          <div class="container"> 
-            <div class="headline"><h1><router-link :to="{name: 'product', params: {id: product.product_id, product: product}}">{{ product.product_name }}</router-link></h1></div>
-            <div class="image"><router-link :to="{name: 'product', params: {id: product.product_id, product: product}}"><img :src="'./src/assets/' + product.product_image" /></router-link></div>
-            <div class="desc"><h3>Description about the product:</h3> <p>{{ product.product_description }}</p></div>
-            <div class="price"><h3>Price: {{ product.product_price | currency }}</h3></div>
-            <div class="add"><button class="button" name="addToCart" @click="addToCart(product)">Add to cart</button></div>
-          </div>  
+      <button
+        class="listgrid"
+        v-on:click="layout = 'list'"
+        v-bind:class="{ 'active': layout == 'list'}"
+        title="List"
+      >List View</button>
+      <button
+        class="listgrid"
+        v-on:click="layout = 'grid'"
+        v-bind:class="{ 'active': layout == 'grid'}"
+        title="Grid"
+      >Grid View</button>
+      <div v-if="layout === 'list'" class="list">
+        <div v-for="(product, index) in products" :key="index">
+          <div class="container">
+            <div class="headline">
+              <h1>
+                <router-link
+                  :to="{name: 'product', params: {id: product.product_id, product: product}}"
+                >{{ product.product_name }}</router-link>
+              </h1>
+            </div>
+            <div class="image">
+              <router-link
+                :to="{name: 'product', params: {id: product.product_id, product: product}}"
+              >
+                <img :src="'./src/assets/' + product.product_image">
+              </router-link>
+            </div>
+            <div class="desc">
+              <h3>Description about the product:</h3>
+              <p>{{ product.product_description }}</p>
+            </div>
+            <div class="price">
+              <h3>Price: {{ product.product_price | currency }}</h3>
+            </div>
+            <div class="add">
+              <button class="button" name="addToCart" @click="addToCart(product)">Add to cart</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div v-if="layout === 'grid'" class="grid">
+        <div class="row">
+          <div v-for="(product, index) in products" :key="index">
+            <div class="containerGrid">
+              <div class="headline">
+                <h1>
+                  <router-link
+                    :to="{name: 'product', params: {id: product.product_id, product: product}}"
+                  >{{ product.product_name }}</router-link>
+                </h1>
+              </div>
+              <div class="image">
+                <router-link
+                  :to="{name: 'product', params: {id: product.product_id, product: product}}"
+                >
+                  <img :src="'./src/assets/' + product.product_image">
+                </router-link>
+              </div>
+              <div class="desc">
+                <h3>Description about the product:</h3>
+                <p>{{ product.product_description }}</p>
+              </div>
+              <div class="price">
+                <h3>Price: {{ product.product_price | currency }}</h3>
+              </div>
+              <div class="add">
+                <button class="button" name="addToCart" @click="addToCart(product)">Add to cart</button>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
     <div class="gridItem two">
-        <app-cart></app-cart>
+      <app-cart></app-cart>
     </div>
   </div>
 </template>
@@ -27,12 +92,15 @@ export default {
   },
   data() {
     return {
-      products: Store.$data.products
+      products: Store.$data.products,
+      layout: "list"
     };
   },
   methods: {
     getProducts() {
-      this.$http.get("http://localhost:3001/products").then(res => {
+      this.$http
+        .get("http://localhost:3001/products")
+        .then(res => {
           this.products = res.data;
         })
         .catch(err => {
@@ -40,7 +108,7 @@ export default {
         });
     },
     addToCart(product) {
-      Store.addToCart(product)
+      Store.addToCart(product);
     }
   },
   mounted() {
@@ -61,52 +129,68 @@ export default {
   grid-column: span 2;
   /*background-color: rgb(25, 25, 61);*/
 }
-h1{
-    font-weight: 900;
+h1 {
+  font-weight: 900;
 }
 .routerlink {
-    text-decoration: none;
-    color: white;
+  text-decoration: none;
+  color: white;
 }
 .container {
-    width: 100%;
-    height: auto;
-    margin-bottom: 2em;
-    margin-top: 2em;
-    margin-left: 1em;
-    border: 5px solid black;
-    padding-top: 1em;
-    padding-bottom: 1em;
+  width: 100%;
+  height: auto;
+  margin-bottom: 2em;
+  margin-top: 2em;
+  margin-left: 1em;
+  border: 5px solid black;
+  padding-top: 1em;
+  padding-bottom: 1em;
+}
+.row {
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: center;
+}
+.containerGrid {
+  height: auto;
+  margin-bottom: 2em;
+  margin-top: 2em;
+  margin-left: 1em;
+  border: 5px solid black;
+  padding-top: 1em;
+  padding-bottom: 1em;
+  padding: 1em;
 }
 .headline {
-    width: 100%;
-    height: auto;
-    margin-bottom: 1em;
+  width: 100%;
+  height: auto;
+  margin-bottom: 1em;
 }
 .image {
-    width: 100%;
-    height: auto;
+  width: 100%;
+  height: auto;
 }
 .desc {
-    width: 100%;
-    height: auto;
-    margin-top: 1em;
+  width: 100%;
+  height: auto;
+  margin-top: 1em;
 }
 .desc p {
-    margin-top: 1em;
+  margin-top: 1em;
 }
 .price {
-    width: 100%;
-    height: auto;
-    margin-top: 1em;
+  width: 100%;
+  height: auto;
+  margin-top: 1em;
 }
 .add {
-    width: 100%;
-    height: auto;
-    margin-top: 1em;
+  width: 100%;
+  height: auto;
+  margin-top: 1em;
 }
 .button {
-  background-color: #4CAF50; /* Green */
+  background-color: #4caf50; /* Green */
   border: none;
   color: white;
   padding: 15px 32px;
@@ -116,6 +200,18 @@ h1{
   font-size: 24px;
   margin: 4px 2px;
   cursor: pointer;
+}
+.listgrid {
+  background-color: rgba(8, 98, 172, 0.986);
+  border: none;
+  color: white;
+  padding: 10px 10px;
+  text-align: center;
+  text-decoration: none;
+  font-size: 24px;
+  cursor: pointer;
+  margin-bottom: 1em;
+  margin-left: 1em;
 }
 .two {
   grid-template-columns: span 1;
